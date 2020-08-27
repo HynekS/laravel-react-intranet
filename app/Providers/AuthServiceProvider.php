149a3f3fe3c3805bcc19e4606d68e0d015b8e74e
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:31:49 +0200

    added token expiration setting

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(60));
     }
 }
