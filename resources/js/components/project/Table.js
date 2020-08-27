ad4372598366b1b9473bc813d0c04ca24c77b600
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:45:29 +0200

    added react-fluid-table component

--- resources/js/components/project/Table.js
+++ resources/js/components/project/Table.js
@@ -1,33 +1,136 @@
 import React, { useState, useEffect } from "react"
-import { useLocation, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { Table as FluidTable } from "react-fluid-table"
+import { Link } from "react-router-dom"
+import styled from "@emotion/styled"
+import tw from "twin.macro"
+import { CSSTransition } from "react-transition-group"
+import orderBy from "lodash.orderby"
 
-import client from "../../utils/axiosWithDefaults"
+import budgetCellRenderer from "./BudgetCellRenderer"
 
-const Table = props => {
-  const [data, setData] = useState()
-  let location = useLocation()
-  let params = useParams() || {}
+const transitionName = `example`
+const appearDuration = 400
 
-  console.log({ location, params })
-  console.log({ ...props })
+const StyledFluidTableWrapper = styled.div`
+  ${tw`px-2 py-1`}
+  &.${transitionName}-appear {
+    opacity: 0;
+}
+
+&.${transitionName}-appear-active {
+    opacity: 1;
+    transition: opacity ${appearDuration}ms ease-out;
+}
+`
+
+const StyledFluidTable = styled(FluidTable)`
+  &.react-fluid-table {
+    ${tw`bg-gray-100 text-sm break-words`};
+  }
+  & .react-fluid-table-header {
+    ${tw`bg-white shadow-md`};
+  }
+  & .row-container {
+    ${tw`transition-colors duration-300`}
+    &:hover {
+      ${tw`bg-gray-200`};
+    }
+  }
+  & .cell {
+    overflow-wrap: anywhere;
+  }
+  & .header-cell-text {
+    font-family: inherit;
+    &.align-right {
+      width: 100%;
+      text-align: right;
+    }
+  }
+`
+const Table = ({ rawData }) => {
+  const [data, setData] = useState([])
+  const { year } = useParams() || {}
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await client.get(`akce/${params.year}`)
-        setData(response)
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    fetchData()
-    return () => {}
-  }, [params])
+    setData(rawData)
+  }, [rawData])
+
+  const onSort = (col, dir) => {
+    setData(!col || !dir ? rawData : orderBy(data, [col], [dir.toLowerCase()]))
+  }
+
+  const columns = [
+    {
+      header: ({ onClick }) => (
+        <div className="header-cell">
+          <span onClick={onClick}>Číslo</span>
+          <input onChange={e => console.log(e.target.value)} />
+        </div>
+      ),
+      key: "c_akce",
+      width: 80,
+      sortable: true,
+      cell: ({ row }) => (
+        <div>
+          <div>{row.c_akce}</div>
+          <Link to={`/akce/${year}/${row.c_akce.split("/")[0]}`} state={row}>
+            Edit
+          </Link>
+        </div>
+      ),
+    },
+    {
+      header: "Název",
+      key: "nazev_akce",
+      width: 240,
+      sortable: true,
+    },
+    {
+      header: "Stav",
+      key: "id_stav",
+      width: 60,
+      sortable: true,
+    },
+    {
+      header: ({ onClick }) => <div className="header-cell">Dohledy</div>,
+      key: "rozpocet_B",
+      width: 100,
+      cell: ({ row }) => budgetCellRenderer({ row, key: "rozpocet_B" }),
+    },
+    {
+      header: ({ onClick }) => <div className="header-cell">Výzkum</div>,
+      key: "rozpocet_A",
+      width: 100,
+      cell: ({ row }) => budgetCellRenderer({ row, key: "rozpocet_A" }),
+    },
+  ]
 
   return (
     <div>
-      <h1>Table</h1>
-      {data && JSON.stringify(data)}
+      {!!data.length && (
+        <CSSTransition
+          in={Boolean(data.length)}
+          classNames={transitionName}
+          timeout={appearDuration}
+          appear={true}
+        >
+          <StyledFluidTableWrapper
+            style={{
+              height: (() =>
+                window.innerHeight - (document.getElementById("header").offsetHeight || 57))(),
+            }}
+          >
+            <StyledFluidTable
+              data={data}
+              columns={columns}
+              estimatedRowHeight={150}
+              onSort={onSort}
+              sortDirection="ASC"
+            />
+          </StyledFluidTableWrapper>
+        </CSSTransition>
+      )}
     </div>
   )
 }
