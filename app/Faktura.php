0a1233f5471b00cb9595ff257945d4476a377077
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:17:17 +0200

    initial commit

--- app/Faktura.php
+++ app/Faktura.php
@@ -5,14 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Faktura extends Model
+{
+    protected $guarded = ['id_zaznam'];
+    protected $table = 'faktury';
+    protected $primaryKey = 'id_zaznam';
+    public $timestamps = false;
+
+    public function akce()
     {
-        protected $guarded = ['id_zaznam'];
-        protected $table = 'faktury';
-        protected $primaryKey = 'id_zaznam';
-        public $timestamps = false;
-        
-        public function akce()
-        {
-            return $this->belongsTo('App\Akce', 'akce_id');
-        }
+        return $this->belongsTo('App\Akce', 'akce_id');
+    }
 }
