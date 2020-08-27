6cccf66a4972627f591c79d15acd4dd2d55ba78c
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:43:47 +0200

    added spinner on pending state

--- resources/js/components/login/LoginForm.js
+++ resources/js/components/login/LoginForm.js
@@ -13,16 +13,17 @@ const FormContainer = tw.div`w-full max-w-xs relative z-10`
 const Form = tw.form`bg-white shadow-xl rounded px-10 pt-8 pb-10 mb-4`
 const Label = tw.label`block text-gray-700 text-sm font-bold mb-2`
 const Input = tw.input`bg-gray-200 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:transition-shadow focus:duration-300`
-const Submit = tw.input`bg-blue-600 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline focus:transition-shadow focus:duration-300`
+const Submit = tw.button`bg-blue-600 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline focus:transition-shadow focus:duration-300`
 const ErrorMessage = tw.div`text-red-600 text-sm pb-2`
 
 const LoginForm = () => {
+  const isAuthPending = useSelector(store => store.auth.isAuthPending)
   const error = useSelector(store => store.auth.authError)
   const dispatch = useDispatch()
   const { register, handleSubmit, errors } = useForm()
   const uid = useState(() => uniqueId())
   const onSubmit = (data, e) => dispatch(submitLoginData(data))
-  
+
   return (
     <FormContainer>
       <Form onSubmit={handleSubmit(onSubmit)}>
@@ -54,7 +55,9 @@ const LoginForm = () => {
         <HiddenMessage show={errors.password}>
           <ErrorMessage>Zadejte, prosím, heslo.</ErrorMessage>
         </HiddenMessage>
-        <Submit type="submit" value="Přihlásit" />
+        <Submit type="submit" className={`${isAuthPending ? "spinner" : ""}`}>
+          Přihlásit
+        </Submit>
       </Form>
     </FormContainer>
   )
