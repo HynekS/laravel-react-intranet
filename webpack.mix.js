df7c742b2aaeb479b40f6bb5cdd5c150523686cb
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:14:35 +0200

    added cleanup function

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,19 +11,52 @@ require("laravel-mix-bundle-analyzer")
  | file for the application as well as bundling up all the JS files.
  |
  */
+const RemovePlugin = require("remove-files-webpack-plugin")
+
+const removePlugin = new RemovePlugin({
+  before: {
+    test: [
+      {
+        folder: "public",
+        method: filePath => {
+          return new RegExp(/(?:.*\.js|.*\.map|mix-manifest\.json)$/, "m").test(filePath)
+        },
+      },
+      {
+        folder: "public/js",
+        method: filePath => {
+          return new RegExp(/(?:.*\.js|.*\.map)$/, "m").test(filePath)
+        },
+        recursive: true,
+      },
+      {
+        folder: "public/css",
+        method: filePath => {
+          return new RegExp(/(?:.*\.css|.*\.map)$/, "m").test(filePath)
+        },
+      },
+    ],
+  },
+
+  after: {},
+})
 
 mix
+  .webpackConfig({
+    plugins: [removePlugin],
+  })
   .options({
-    //terser: {
-    //  extractComments: false,
-    //},
+    terser: {
+      extractComments: false,
+    },
     hmrOptions: {
       host: "127.0.0.1",
       port: 8082,
     },
   })
   .react("resources/js/app.js", "public/js")
+  .version()
   .copyDirectory("resources/images/", "public/images/", false)
-  // .bundleAnalyzer()
-  .sourceMaps()
+  .bundleAnalyzer()
+  .sourceMaps(false, "source-map")
   .browserSync("localhost:8000")
