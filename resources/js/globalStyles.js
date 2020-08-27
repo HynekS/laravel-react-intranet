a7ede92953d4900b36e4b4ac99fe5dee7c26ce4c
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:36:22 +0200

    initial commit

---
+++ resources/js/globalStyles.js
@@ -0,0 +1,36 @@
+export default function globalStyles(props) {
+  return `html {
+    color: #555;
+    overflow-y: overlay;
+    -ms-overflow-style: -ms-autohiding-scrollbar;
+  }
+  .spinner {
+    position: relative;
+    color: transparent !important;
+    pointer-events: none;
+  }
+
+  .spinner::after {
+    content: "";
+    position: absolute !important;
+    top: calc(50% - (1em / 2));
+    left: calc(50% - (1em / 2));
+    display: block;
+    width: 1em;
+    height: 1em;
+    border: 2px solid #fff;
+    border-radius: 9999px;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    animation: spinAround 500ms infinite linear;
+  }
+
+  @keyframes spinAround {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+}`
+}
