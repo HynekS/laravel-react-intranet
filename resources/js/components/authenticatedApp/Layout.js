719201629e3f48341522f29c2cbb177b9df68938
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:38:48 +0200

    wrapped imports to react.lazy, added suspense

--- resources/js/components/authenticatedApp/Layout.js
+++ resources/js/components/authenticatedApp/Layout.js
@@ -4,21 +4,25 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./Header"
 import Nav from "./Nav"
 import Main from "./Main"
-import Table from "../project/Table"
 import Dashboard from "../dashboard/Dashboard"
+const TableDataProvider = React.lazy(() => import("../project/TableDataProvider"))
+const DetailProvider = React.lazy(() => import("../project/DetailProvider"))
 
 const Layout = props => {
   return (
     <div>
       <Header>
-        <Nav></Nav>
+        <Nav />
       </Header>
       <Main>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/akce" element={<Table />} />
-          <Route path="/akce/:year" element={<Table />} />
-        </Routes>
+        <React.Suspense fallback={<div>I aM tHe SusPEnSE !!!</div>}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/akce" element={<TableDataProvider />} />
+            <Route path="/akce/:year" element={<TableDataProvider />} />
+            <Route path="/akce/:year/:num/*" element={<DetailProvider />} />
+          </Routes>
+        </React.Suspense>
       </Main>
     </div>
   )
