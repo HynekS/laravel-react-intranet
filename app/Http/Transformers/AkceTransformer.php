01aa7e8187c709c5e68d9ae6458f06cfa3652b92
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:28:34 +0200

    initial commit

---
+++ app/Http/Transformers/AkceTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Transformers;
+
+use Carbon\Carbon;
+
+class AkceTransformer
+{
+  public static function transformResponse($response)
+  {
+    is_null($response->nalez) ? $response->nalez = "null" : $response->nalez = strval($response->nalez);
+
+    $response->datum_pocatku_text = self::tryToParseDate($response->datum_pocatku_text);
+    $response->datum_ukonceni_text = self::tryToParseDate($response->datum_ukonceni_text);
+    $response->datum_pocatku = self::tryToParseDate($response->datum_pocatku);
+    $response->datum_ukonceni = self::tryToParseDate($response->datum_ukonceni);
+
+    return $response;
+  }
+
+  public static function transformRequest($request)
+  {
+    // Is it neccessary? Would the type be cast anyway?
+    if ($request->nalez == "null") {
+      $request->nalez = null;
+    } else {
+      $request->nalez = (int) $request->nalez;
+    }
+  }
+
+  private static function tryToParseDate($raw_date, $outputFormat = "j. n. Y")
+  {
+    try {
+      return (empty($raw_date) || is_null($raw_date)) ? "" : Carbon::parse($raw_date)->format($outputFormat);
+    } catch (\Exception $err) {
+      return $raw_date;
+    }
+  }
+}
