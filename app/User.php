9acaedc83f149378ef90fc8db8c39902932f7b6d
HynekS <hyneksvacha@seznam.cz>
2020-08-27 18:18:12 +0200

    extended fillable, added "akce' relationship

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'user_name', 'full_name', 'email', 'password', 'role'
     ];
 
     /**
@@ -37,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function akce() {
+        return $this->hasMany('App\Akce', 'user_id');
+    }
 }
