2f2c8a7ad8fe0aeccffc855342d8f8929b00f654
HynekS <hyneksvacha@seznam.cz>
2020-08-28 08:28:24 +0200

    added logout icon, added @jsx flag

--- resources/js/components/authenticatedApp/Logout.js
+++ resources/js/components/authenticatedApp/Logout.js
@@ -1,11 +1,14 @@
+/** @jsx jsx */
 import React from "react"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
+import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
 import { logout } from "../../store/auth"
+import SvgLogout from "../../vendor/heroicons/outline/Logout"
 
-const Button = tw.button`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`
+const Button = tw.button`flex items-center bg-blue-500 hover:bg-blue-700 transition-colors duration-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`
 
 const Logout = () => {
   const dispatch = useDispatch()
@@ -18,7 +21,8 @@ const Logout = () => {
       className={`${pending ? "spinner" : ""}`}
       onClick={() => dispatch(logout(navigate))}
     >
-      Odhlášení
+      <span tw="pr-2">Odhlášení</span>
+      <SvgLogout tw="w-4" />
     </Button>
   )
 }
