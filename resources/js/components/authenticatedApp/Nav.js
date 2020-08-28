0ed6534e3610927242a9f4b7fc722647293d6f71
HynekS <hyneksvacha@seznam.cz>
2020-08-28 08:28:59 +0200

    changed markup, added @jsx flag

--- resources/js/components/authenticatedApp/Nav.js
+++ resources/js/components/authenticatedApp/Nav.js
@@ -1,40 +1,43 @@
-import React, { useState } from "react"
+/** @jsx jsx */
+import React from "react"
 import { NavLink } from "react-router-dom"
 import { useSelector } from "react-redux"
 import styled from "@emotion/styled"
+import { jsx } from '@emotion/core'
 import tw from "twin.macro"
 
 import UserWidget from "./UserWidget"
 import LogoThumb from "../common/LogoThumb"
 import Logout from "./Logout"
 
-const NavContainer = tw.nav`flex justify-between py-2 px-4 border-b`
+const Container = tw.section`flex justify-between py-2 px-4 border-b`
 const StyledNavLink = styled(NavLink)`
   ${tw`p-2 text-blue-500 hover:text-blue-800`}
   &.active {
     ${tw`bg-gray-200`}
   }
 `
+const NavContainer = tw.nav`py-2 px-4`
 
 const Nav = ({ ...props }) => {
   const user = useSelector(store => store.auth.user)
 
   const currentYear = new Date().getFullYear()
   return (
-    <NavContainer role="navigation" aria-label="main navigation">
-      <div>
-        <NavLink to="/">
-          <LogoThumb />
-        </NavLink>
-      </div>
-      <div>
-        <StyledNavLink to={`/akce`}>vše</StyledNavLink>
-        {Array.from({ length: currentYear - 2013 }, (_, i) => currentYear - i).map((year, i) => (
-          <StyledNavLink to={`/akce/${year}`} key={year}>
-            {year}
-          </StyledNavLink>
-        ))}
-      </div>
+    <Container role="navigation" aria-label="main navigation">
+        <div>
+          <NavLink to="/">
+            <LogoThumb />
+          </NavLink>
+        </div>
+        <NavContainer>
+          <StyledNavLink to={`/akce`}>vše</StyledNavLink>
+          {Array.from({ length: currentYear - 2013 }, (_, i) => currentYear - i).map((year, i) => (
+            <StyledNavLink to={`/akce/${year}`} key={year}>
+              {year}
+            </StyledNavLink>
+          ))}
+        </NavContainer>
       <div>
         {user && (
           <UserWidget user={user}>
@@ -42,7 +45,7 @@ const Nav = ({ ...props }) => {
           </UserWidget>
         )}
       </div>
-    </NavContainer>
+    </Container>
   )
 }
 
