437f46bd76ad726f37317e01cb9716d147c54a32
HynekS <hyneksvacha@seznam.cz>
2020-08-28 08:29:40 +0200

    added user styled component

--- resources/js/components/authenticatedApp/UserWidget.js
+++ resources/js/components/authenticatedApp/UserWidget.js
@@ -1,12 +1,13 @@
 import React from "react"
 import tw from "twin.macro"
 
-const Widget = tw.div`flex`
+const Widget = tw.section`flex`
+const User = tw.section`py-2 px-4`
 
 const UserWidget = ({ user, children }) => {
   return (
     <Widget>
-      <p>Přihlášený uživatel: {user.full_name}</p>
+      <User>Přihlášený uživatel: {user.full_name}</User>
       {children}
     </Widget>
   )
