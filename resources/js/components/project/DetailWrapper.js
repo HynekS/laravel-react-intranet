47d561049657e212eb44305b81fb73797b285941
HynekS <hyneksvacha@seznam.cz>
2020-09-03 10:08:41 +0200

    initial commit

---
+++ resources/js/components/project/DetailWrapper.js
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "@emotion/core"
+import tw from "twin.macro"
+
+const DetailWrapper = ({ children }) => <div tw="py-2 px-10 bg-gray-100">{children}</div>
+
+export default DetailWrapper
