17cbb7b056a7e0ef6b0f1c383c5926272cc8c91f
HynekS <hyneksvacha@seznam.cz>
2020-09-03 10:09:28 +0200

    added custom fills and strokes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
-  }
+    stroke: theme => ({
+      current: "currentColor",
+      "red-400": theme("colors.red.400"),
+      "green-400": theme("colors.green.400"),
+    }),
+    fill: theme => ({
+      current: "currentColor",
+      "red-100": theme("colors.red.100"),
+      "green-100": theme("colors.green.100"),
+    }),
+    extend: {},
+  },
 }
