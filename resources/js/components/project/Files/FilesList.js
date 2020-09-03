1699683472efdcd165fa3740e874b1cfb258adf8
HynekS <hyneksvacha@seznam.cz>
2020-09-03 10:27:56 +0200

    removed dead code

--- resources/js/components/project/Files/FilesList.js
+++ resources/js/components/project/Files/FilesList.js
@@ -1,69 +1,28 @@
-import React, { useState, useEffect } from "react";
-import ProjectFilesFile from "./File";
+import React, { useState, useEffect } from "react"
+import File from "./File"
 // import FileUpload from "../FileUpload/FileUpload";
-import Modal from "react-modal";
+import ReactModal from "react-modal"
+import { useModal } from "react-modal-hook"
 
 const FilesList = ({ subgroup, detail, ...props }) => {
-  const [isModalOpen, toggleIsModalOpen] = useState(false);
+  const [showModal, hideModal] = useModal(() => (
+    <ReactModal isOpen>
+      <p>Modal content</p>
+      <button onClick={hideModal}>Hide modal</button>
+    </ReactModal>
+  ))
 
-  useEffect(() => {
-    Modal.setAppElement("#app");
-  }, []);
-  
   return (
     <div>
       <div>
-        <h4 className="title is-7">{subgroup.publicName}</h4>
-        <div>
-          {subgroup.data.length > 0 &&
-            subgroup.data.map((file, i) => (
-              <ProjectFilesFile file={file} key={i} />
-            ))}
-          <button
-           
-            onClick={() => toggleIsModalOpen(!isModalOpen)}
-          >
-            Nahrát soubory
-          </button>
+        <h4>{subgroup.publicName}</h4>
+        <div style={{ backgroundColor: "rgb(247, 250, 252)"}}>
+          {subgroup.data.length > 0 && subgroup.data.map((file, i) => <File file={file} key={i} />)}
+          <button onClick={showModal}>Nahrát soubory</button>
         </div>
       </div>
-      {/*<Modal
-        isOpen={isModalOpen}
-        shouldCloseOnOverlayClick={true}
-        onRequestClose={() => toggleIsModalOpen(!isModalOpen)}
-        contentLabel="Example Modal"
-        className={`modal ${isModalOpen ? "is-active" : ""}`}
-        overlayClassName="modal-background"
-      >
-        <div>
-          <header>
-            <p>
-              nahrát: {subgroup.publicName}
-            </p>
-            <button
-             
-              aria-label="close"
-              onClick={() => toggleIsModalOpen(!isModalOpen)}
-            />
-          </header>
-          <section>
-            <FileUpload model={subgroup.key} id={detail.id_akce} />
-          </section>
-          <footer>
-            <div>
-              <button>Nahrát soubory</button>
-              <button
-               
-                onClick={() => toggleIsModalOpen(!isModalOpen)}
-              >
-                Zrušit
-              </button>
-            </div>
-          </footer>
-        </div>
-      </Modal>*/}
     </div>
-  );
-};
+  )
+}
 
-export default FilesList;
+export default FilesList
