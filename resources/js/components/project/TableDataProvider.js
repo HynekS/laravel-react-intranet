0d3e184d8d3e10797dc0e0e96a1e76edefbfa423
HynekS <hyneksvacha@seznam.cz>
2020-09-05 22:37:42 +0200

    data fetching moved to redux

--- resources/js/components/project/TableDataProvider.js
+++ resources/js/components/project/TableDataProvider.js
@@ -1,14 +1,34 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { createSelector } from "reselect"
 
-import client from "../../utils/axiosWithDefaults"
-// import Table from "./Table"
 import BaseTable from "./BaseTable"
-
-import { loadProgressBar } from "axios-progress-bar"
 import "axios-progress-bar/dist/nprogress.css"
+import { fetchProjectsOfOneYear } from "../../store/projects"
+
+const getProjectIdsByYear = (state, year) => state.projects.IdsByYear[year]
+const getProjectsById = state => state.projects.byId
+
+const test = createSelector(
+  getProjectIdsByYear,
+  getProjectsById,
+  (projectIdsByYear, projectsById) => (projectIdsByYear || []).map(id => projectsById[id]),
+)
 
 const TableDataProvider = props => {
+  const dispatch = useDispatch()
+  const { year } = useParams() || {}
+
+  const projectsOfOneYear = useSelector(state => test(state, year))
+
+  useEffect(() => {
+    if (!projectsOfOneYear.length) {
+      dispatch(fetchProjectsOfOneYear(year))
+    }
+  }, [year])
+
+  /*
   const [data, setData] = useState(null)
   let { year } = useParams() || {}
 
@@ -35,9 +55,10 @@ const TableDataProvider = props => {
       loadProgressBar({ progress: false })
     }
   }, [year])
-
-  // return data ? <Table rawData={data} {...props} /> : "Loading…"
+  
   return data ? <BaseTable rawData={data} {...props} /> : "Loading…"
+  */
+  return projectsOfOneYear ? <BaseTable rawData={projectsOfOneYear} {...props} /> : "Loading…"
 }
 
 export default TableDataProvider
