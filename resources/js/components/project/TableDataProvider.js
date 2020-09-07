3e0df2e15bebf4b6780d009df5b60be7c1128fcd
HynekS <hyneksvacha@seznam.cz>
2020-09-07 08:07:07 +0200

    Added logic for fetching all project

--- resources/js/components/project/TableDataProvider.js
+++ resources/js/components/project/TableDataProvider.js
@@ -5,59 +5,60 @@ import { createSelector } from "reselect"
 
 import BaseTable from "./BaseTable"
 import "axios-progress-bar/dist/nprogress.css"
-import { fetchProjectsOfOneYear } from "../../store/projects"
+import { fetchProjectsOfOneYear, fetchAllProjects } from "../../store/projects"
+
+const getYear = (_, year) => year
 
 const getProjectIdsByYear = (state, year) => state.projects.IdsByYear[year]
 const getProjectsById = state => state.projects.byId
 
-const test = createSelector(
+const getIsFetchingAll = state => state.projects.isFetchingAll
+const getIsAllFetched = state => state.projects.isAllFetched
+
+const singleYearSelector = createSelector(
   getProjectIdsByYear,
   getProjectsById,
   (projectIdsByYear, projectsById) => (projectIdsByYear || []).map(id => projectsById[id]),
+)
+
+// TODO clear up selector || render conditions, there are likely redundancies
+const allProjectsSelector = createSelector(
+  getIsFetchingAll,
+  getIsAllFetched,
+  getYear,
+  (isFetchingAll, isAllFetched, year) => isFetchingAll || (isAllFetched && !year),
 )
 
 const TableDataProvider = props => {
   const dispatch = useDispatch()
   const { year } = useParams() || {}
 
-  const projectsOfOneYear = useSelector(state => test(state, year))
+  const projectsOfOneYear = useSelector(state => singleYearSelector(state, year))
+  const allProjects = useSelector(state => allProjectsSelector(state, year))
+
+  const isAllFetched = useSelector(state => state.projects.isAllFetched)
+  const allById = useSelector(state => state.projects.byId)
 
   useEffect(() => {
-    if (!projectsOfOneYear.length) {
+    /* If the url is '/akce', then fetch all projects year after year.
+    TODO: add check if there are't some already to save bandwidth.
+    The 'else if' clause is not optimal, but it prevent the execution
+    of both conditions. I want to avoid return statement, because
+    it has special meaning inside useEffect function. */
+    if (year === undefined) {
+      if (!isAllFetched) dispatch(fetchAllProjects())
+    } else if (!projectsOfOneYear.length) {
       dispatch(fetchProjectsOfOneYear(year))
     }
   }, [year])
 
   /*
-  const [data, setData] = useState(null)
-  let { year } = useParams() || {}
-
-  useEffect(() => {
-    let source = client.CancelToken.source()
-    async function fetchData() {
-      try {
-        loadProgressBar({}, client)
-        const response = await client.get(`akce/${year == undefined ? `` : year}`, {
-          cancelToken: source.token,
-        })
-        setData(response.data)
-      } catch (err) {
-        if (client.isCancel(err)) {
-          console.log("api request cancelled")
-        } else {
-          console.log(err)
-        }
-      }
-    }
-    fetchData()
-    return () => {
-      source.cancel()
-      loadProgressBar({ progress: false })
-    }
-  }, [year])
-  
-  return data ? <BaseTable rawData={data} {...props} /> : "Loading…"
+  TODO Handle better case where year has no projects (it doesn't break anything – the error is caught
+  and the result is blank table, which is not bad, but a message would be better.)
   */
+  if (allProjects) {
+    return isAllFetched ? <BaseTable rawData={Object.values(allById)} {...props} /> : "Loading…"
+  }
   return projectsOfOneYear ? <BaseTable rawData={projectsOfOneYear} {...props} /> : "Loading…"
 }
 
