17f2cb93c4e151be2f08b457ecd07313d563eab8
HynekS <hyneksvacha@seznam.cz>
2020-09-10 07:10:54 +0200

    restored original styled components, slighly upated styled (naimly transitions)

--- resources/js/components/common/StyledModal.js
+++ resources/js/components/common/StyledModal.js
@@ -1,11 +1,9 @@
 import React from "react"
 import ReactModal from "react-modal"
-// import styled from "@emotion/styled"
-import "./StyledModal.css"
+import styled from "@emotion/styled"
 
 ReactModal.setAppElement("#app")
 
-/*
 function ReactModalAdapter({ className, ...props }) {
   const contentClassName = `${className}__content`
   const overlayClassName = `${className}__overlay`
@@ -26,14 +24,17 @@ const StyledReactModal = styled(ReactModalAdapter)`
     left: 0px;
     right: 0px;
     bottom: 0px;
-    background-color: rgba(0, 0, 0, 0.5);
-    opacity: 0;
-    transition: opacity 500ms ease-in-out;
+    &.ReactModal__Overlay {
+      background-color: rgba(0, 0, 0, 0.5);
+      opacity: 0;
+      transition: opacity 300ms ease-out;
+    }
     &.ReactModal__Overlay--after-open {
       opacity: 1;
     }
     &.ReactModal__Overlay--before-close {
       opacity: 0;
+      transition: opacity 200ms ease-out;
     }
   }
 
@@ -43,49 +44,26 @@ const StyledReactModal = styled(ReactModalAdapter)`
     left: 40px;
     right: 40px;
     bottom: 40px;
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.25);
     background: #fff;
     overflow: auto;
-    -webkit-overflow-scrolling: touch;
     border-radius: 4px;
     outline: none;
     padding: 20px;
-    transition: transform 500ms ease-in-out;
-    transform: translateY(100px);
-    &.ReactModal__Content--after-open {
-      transform: translateY(0px);
+    &.ReactModal__Content {
+      box-shadow: 0 0 1rem rgba(0, 0, 0, 0.25);
+      transform: translateY(1rem);
+      transition: transform 300ms ease-out;
+      -webkit-overflow-scrolling: touch;
     }
+
+    &.ReactModal__Content--after-open {
+      transform: translateY(0);
+    }
+
     &.ReactModal__Content--before-close {
-      transform: translateY(100px);
+      transform: translateY(1rem);
+      transition: transform 200ms ease-out;
     }
   }
 `
-*/
-// export default StyledReactModal
-
-ReactModal.defaultStyles = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(74, 85, 104, 0.75)'
-  },
-  content: {
-    position: 'absolute',
-    top: '40px',
-    left: '40px',
-    right: '40px',
-    bottom: '40px',
-    background: '#fff',
-    overflow: 'auto',
-    WebkitOverflowScrolling: 'touch',
-    borderRadius: '4px',
-    outline: 'none',
-    padding: '20px'
-  }
-}
-
-
-export default ReactModal
+export default StyledReactModal
