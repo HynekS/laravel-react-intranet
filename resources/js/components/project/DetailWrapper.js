60c04f309103ec88c12a701df312ae13e2e6aa0b
HynekS <hyneksvacha@seznam.cz>
2020-09-10 07:14:26 +0200

    added inner shadow, darkened background

--- resources/js/components/project/DetailWrapper.js
+++ resources/js/components/project/DetailWrapper.js
@@ -3,6 +3,6 @@ import React from "react"
 import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
-const DetailWrapper = ({ children }) => <div tw="py-2 px-10 bg-gray-100">{children}</div>
+const DetailWrapper = ({ children }) => <div tw="py-2 px-10 bg-gray-200 shadow-inner">{children}</div>
 
 export default DetailWrapper
