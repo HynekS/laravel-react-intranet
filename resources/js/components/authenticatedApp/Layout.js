88e570b33f32005abf1387e59c331fe11da9909b
HynekS <hyneksvacha@seznam.cz>
2020-09-11 08:47:13 +0200

    added flex wrapper components to allow content fit viewport

--- resources/js/components/authenticatedApp/Layout.js
+++ resources/js/components/authenticatedApp/Layout.js
@@ -5,26 +5,33 @@ import Header from "./Header"
 import Nav from "./Nav"
 import Main from "./Main"
 import Dashboard from "../dashboard/Dashboard"
+import RootFlexWrapper from "./RootFlexWrapper"
+import HeaderFlexWrapper from "./HeaderFlexWrapper"
+import ContentFlexWrapper from "./ContentFlexWrapper"
 const TableDataProvider = React.lazy(() => import("../project/TableDataProvider"))
 const DetailProvider = React.lazy(() => import("../project/DetailProvider"))
 
 const Layout = props => {
   return (
-    <div>
-      <Header>
-        <Nav />
-      </Header>
-      <Main>
-        <React.Suspense fallback={<div>I aM tHe SusPEnSE !!!</div>}>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/akce" element={<TableDataProvider />} />
-            <Route path="/akce/:year" element={<TableDataProvider />} />
-            <Route path="/akce/:year/:num/*" element={<DetailProvider />} />
-          </Routes>
-        </React.Suspense>
-      </Main>
-    </div>
+    <RootFlexWrapper>
+      <HeaderFlexWrapper>
+        <Header>
+          <Nav />
+        </Header>
+      </HeaderFlexWrapper>
+      <ContentFlexWrapper>
+        <Main>
+          <React.Suspense fallback={<div>I aM tHe SusPEnSE !!!</div>}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/akce" element={<TableDataProvider />} />
+              <Route path="/akce/:year" element={<TableDataProvider />} />
+              <Route path="/akce/:year/:num/*" element={<DetailProvider />} />
+            </Routes>
+          </React.Suspense>
+        </Main>
+      </ContentFlexWrapper>
+    </RootFlexWrapper>
   )
 }
 
