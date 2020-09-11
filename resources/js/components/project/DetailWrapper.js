7292a5ef369104306d93a89cd07745c34889cbab
HynekS <hyneksvacha@seznam.cz>
2020-09-11 08:52:38 +0200

    added full width class because of flex parent

--- resources/js/components/project/DetailWrapper.js
+++ resources/js/components/project/DetailWrapper.js
@@ -3,6 +3,6 @@ import React from "react"
 import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
-const DetailWrapper = ({ children }) => <div tw="py-2 px-10 bg-gray-200 shadow-inner">{children}</div>
+const DetailWrapper = ({ children }) => <div tw="w-full py-2 px-10 bg-gray-200 shadow-inner">{children}</div>
 
 export default DetailWrapper
