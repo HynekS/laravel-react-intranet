a6adbb0d0a9f59e874ab495448f357b51b710058
HynekS <hyneksvacha@seznam.cz>
2020-09-12 20:09:03 +0200

    imported tailwind, minor style update

--- resources/js/components/project/Files/FilesGroup.js
+++ resources/js/components/project/Files/FilesGroup.js
@@ -1,10 +1,14 @@
+/** @jsx jsx */
 import React from "react"
+import { jsx } from "@emotion/core"
+import tw from "twin.macro"
+
 import FilesList from "./FilesList"
 
 const ProjectFilesGroup = ({ group, label, detail, ...props }) => {
   return (
     <div>
-      <h3>{label}</h3>
+      <h3 tw="font-bold">{label}</h3>
       {!!group.length &&
         group.map((item, i) => <FilesList subgroup={item} detail={detail} {...props} key={i} />)}
     </div>
