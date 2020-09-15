db69a7cb803bff9610a38ca60d22c2f6a7ea0b9f
HynekS <hyneksvacha@seznam.cz>
2020-09-15 10:12:08 +0200

    added 'gray-400' fill property

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       fill: theme => ({
         "red-100": theme("colors.red.100"),
         "green-100": theme("colors.green.100"),
+        "gray-400": theme("colors.gray.400"),
       }),
       inset: {
         "2": "0.5rem",
