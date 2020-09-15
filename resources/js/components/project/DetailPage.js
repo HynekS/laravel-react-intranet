41755ca598c495cd4dc81e675efafcb2303758eb
HynekS <hyneksvacha@seznam.cz>
2020-09-15 10:04:18 +0200

    initial commit

---
+++ resources/js/components/project/DetailPage.js
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "@emotion/core"
+import tw from "twin.macro"
+
+const DetailPage = ({ children }) => <div tw="w-full py-2 px-10 bg-gray-400 shadow-inner">{children}</div>
+
+export default DetailPage
