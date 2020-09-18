fb6f93276e039bc91e40f50be6c8754bfed0e968
HynekS <hyneksvacha@seznam.cz>
2020-09-18 16:51:02 +0200

    added styles to container element

--- resources/js/components/authenticatedApp/Main.js
+++ resources/js/components/authenticatedApp/Main.js
@@ -4,7 +4,7 @@ import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
 const Main = ({ children }) => {
-  return <main tw="flex flex-auto">{children}</main>
+  return <main tw="flex flex-auto bg-gray-400 shadow-inner">{children}</main>
 }
 
 export default Main
