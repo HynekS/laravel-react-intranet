91aa28a30f6ea2ffc75bee4b94078f6f3e3c4b19
HynekS <hyneksvacha@seznam.cz>
2020-09-20 12:23:11 +0200

    updated paths to renamed components

--- resources/js/components/project/lazyImports.js
+++ resources/js/components/project/lazyImports.js
@@ -3,6 +3,6 @@ import lazy from "react-lazy-with-preload"
 const Detail = lazy(() => import("./Detail"))
 const Invoices = lazy(() => import("./Invoices/Invoices"))
 const ExpertSheet = lazy(() => import("./ExpertSheet"))
-const Files = lazy(() => import("./Files/Files"))
+const Files = lazy(() => import("./Files/FilesProvider"))
 
 export { Detail, Invoices, ExpertSheet, Files }
