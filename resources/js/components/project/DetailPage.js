f6a56d3651b38d79086ee623780d33adc1ea27ba
HynekS <hyneksvacha@seznam.cz>
2020-09-20 12:17:47 +0200

    removed a bit of styles which were not playing nice with other components

--- resources/js/components/project/DetailPage.js
+++ resources/js/components/project/DetailPage.js
@@ -3,6 +3,6 @@ import React from "react"
 import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
-const DetailPage = ({ children }) => <div tw="w-full py-2 px-10 bg-gray-400 shadow-inner">{children}</div>
+const DetailPage = ({ children }) => <div tw="w-full py-2 px-2 bg-gray-400 shadow-inner lg:(px-10)">{children}</div>
 
 export default DetailPage
