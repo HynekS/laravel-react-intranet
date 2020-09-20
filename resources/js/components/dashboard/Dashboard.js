1c3e792a9c71f468bac31e91e8218190aaffeb88
HynekS <hyneksvacha@seznam.cz>
2020-09-20 12:15:52 +0200

    Added first feature mocks

--- resources/js/components/dashboard/Dashboard.js
+++ resources/js/components/dashboard/Dashboard.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+// @ts-check
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "@emotion/core"
+import tw from "twin.macro"
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Link to="/akce/2018">Test</Link>
+    <div tw="w-full p-8">
+      <div tw="p-8 bg-white rounded-lg">
+        <h1 tw="text-xl">Dashboard</h1>
+        <section tw="pb-8">
+          <form action="">
+            <label htmlFor="quicksearch" tw="block text-gray-700 text-sm font-bold mb-2">
+              rychlé hledání
+            </label>
+            <input
+              type="text"
+              name="quicksearch"
+              tw="bg-gray-200 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:transition-shadow focus:duration-300"
+            />
+          </form>
+        </section>
+        <section tw="pb-8">
+          <h2 tw="text-lg">Statistiky</h2>
+        </section>
+      </div>
     </div>
   )
 }
