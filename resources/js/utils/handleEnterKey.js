4788f7d3ecf6719672a35111680df641dcb152ea
HynekS <hyneksvacha@seznam.cz>
2020-09-21 21:23:32 +0200

    added condition to prevent focusing the submit input/button when focus is on the previous sibling form element. Form is now being submitted instead.

--- resources/js/utils/handleEnterKey.js
+++ resources/js/utils/handleEnterKey.js
@@ -1,8 +1,11 @@
 export default function handleEnter(e) {
-  if (e.key === 'Enter' || e.keyCode === 13) {
-    const form = e.target.form;
-    const index = [].indexOf.call(form, e.target);
-    form.elements[index + 1].focus();
-    e.preventDefault();
+  if (e.key === "Enter" || e.keyCode === 13) {
+    const form = e.target.form
+    const index = [].indexOf.call(form, e.target)
+    if (form.elements[index + 1].getAttribute("type") !== "submit") {
+      form.elements[index + 1].focus()
+      // if the next element is submit button, then submit
+      e.preventDefault()
+    }
   }
 }
