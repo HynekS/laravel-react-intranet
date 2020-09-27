5194eaff7a7caa9e0f97cea7db6af43ce14c8bda
HynekS <hyneksvacha@seznam.cz>
2020-09-27 22:54:46 +0200

    extracted componsnt composition to make more reusable

--- resources/js/components/common/Input.js
+++ resources/js/components/common/Input.js
@@ -1,30 +1,39 @@
 import React from "react"
-import tw from "twin.macro"
 
-const StyledInput = tw.input`bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500`
-const StyledLabel = tw.label`block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4`
-const RowWrapper = tw.div`md:flex md:items-center mb-2`
-const LabelWrapper = tw.div`md:w-1/3`
-const InputWrapper = tw.div`md:w-2/3`
+const DefaultRootWrapper = ({ children }) => <div>{children}</div>;
+const DefaultLabelWrapper = ({ children }) => <div>{children}</div>
+const DefaultInputWrapper = ({ children }) => <div>{children}</div>
 
-const Input = ({ name, label, placeholder, id, register, type = "text", ...props }) => {
+const Input = ({
+  name,
+  label,
+  placeholder,
+  register,
+  type = "text",
+  RootWrapperComponent = DefaultRootWrapper,
+  LabelWrapperComponent = DefaultLabelWrapper,
+  InputWrapperComponent = DefaultInputWrapper,
+  LabelComponent = "label",
+  InputComponent = "input",
+  ...props
+}) => {
   return (
-    <RowWrapper>
-      <LabelWrapper>
-        <StyledLabel htmlFor={name}>{label}</StyledLabel>
-      </LabelWrapper>
-      <InputWrapper>
-        <StyledInput
-          id={id}
+    <RootWrapperComponent {...props}>
+      <LabelWrapperComponent {...props}>
+        <LabelComponent htmlFor={name} {...props}>{label}</LabelComponent>
+      </LabelWrapperComponent>
+      <InputWrapperComponent {...props}>
+        <InputComponent
+          id={name}
           type={type}
           name={name}
           placeholder={placeholder}
           ref={register}
           {...props}
         />
-      </InputWrapper>
-    </RowWrapper>
-  )
-}
+      </InputWrapperComponent>
+    </RootWrapperComponent>
+  );
+};
 
-export default Input
+export default Input;
