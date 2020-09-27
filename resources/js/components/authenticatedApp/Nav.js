453626e5f0ff20c9cbcef51962e3eeaf3afa030e
HynekS <hyneksvacha@seznam.cz>
2020-09-27 23:04:12 +0200

    added WIP link to project creating component; added flex wrap to multiple component (temporary fix to prevent broken layout on mobile)

--- resources/js/components/authenticatedApp/Nav.js
+++ resources/js/components/authenticatedApp/Nav.js
@@ -3,21 +3,21 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 import { useSelector } from "react-redux"
 import styled from "@emotion/styled"
-import { jsx } from '@emotion/core'
+import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
 import UserWidget from "./UserWidget"
 import LogoThumb from "../common/LogoThumb"
 import Logout from "./Logout"
 
-const Container = tw.section`flex justify-between py-2 px-4 border-b`
+const Container = tw.section`flex flex-wrap justify-between py-2 px-4 border-b`
 const StyledNavLink = styled(NavLink)`
   ${tw`p-2 text-blue-500 hover:text-blue-800`}
   &.active {
     ${tw`bg-gray-200`}
   }
 `
-const NavContainer = tw.nav`py-2 px-4`
+const NavContainer = tw.nav`py-2 px-4 flex flex-wrap`
 
 const Nav = ({ ...props }) => {
   const user = useSelector(store => store.auth.user)
@@ -25,20 +25,27 @@ const Nav = ({ ...props }) => {
   const currentYear = new Date().getFullYear()
   return (
     <Container role="navigation" aria-label="main navigation">
-        <div>
-          <NavLink to="/" aria-label="návrat na domovskou stránku">
-            <LogoThumb />
-          </NavLink>
-        </div>
-        <NavContainer>
-          <StyledNavLink to={`/akce`}>vše</StyledNavLink>
-          {Array.from({ length: currentYear - 2013 }, (_, i) => currentYear - i).map((year, i) => (
-            <StyledNavLink to={`/akce/${year}`} key={year} aria-label={`akce proběhlé v roce ${year}`}>
-              {year}
-            </StyledNavLink>
-          ))}
-        </NavContainer>
       <div>
+        <NavLink to="/" aria-label="návrat na domovskou stránku">
+          <LogoThumb />
+        </NavLink>
+      </div>
+      <NavContainer>
+        <StyledNavLink to={`/akce`}>vše</StyledNavLink>
+        {Array.from({ length: currentYear - 2013 }, (_, i) => currentYear - i).map((year, i) => (
+          <StyledNavLink
+            to={`/akce/${year}`}
+            key={year}
+            aria-label={`akce proběhlé v roce ${year}`}
+          >
+            {year}
+          </StyledNavLink>
+        ))}
+      </NavContainer>
+      <div>
+        <NavLink to="vytvorit-akci" aria-label="vytvořit novou akci">
+          + vytvořit novou akci
+        </NavLink>
         {user && (
           <UserWidget user={user}>
             <Logout />
