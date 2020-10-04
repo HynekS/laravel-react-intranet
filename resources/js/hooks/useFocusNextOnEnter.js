d8382883e7a6aa11525dfdf436e53476e3836157
HynekS <hyneksvacha@seznam.cz>
2020-10-04 20:50:36 +0200

    initial commit

---
+++ resources/js/hooks/useFocusNextOnEnter.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react"
+
+export default function useFocusNextOnEnter() {
+  const formRef = useRef()
+
+  const isForm = node => Boolean(node.nodeName === "FORM")
+
+  const filterFormElements = el => {
+    if (
+      el.nodeName.toLowerCase() === "fieldset" ||
+      ["submit", "hidden"].includes(el.getAttribute("type"))
+    ) {
+      return false
+    }
+    return true
+  }
+
+  useEffect(() => {
+    const handleKeyPress = e => {
+      if (e.key === "Enter" || e.keyCode === 13) {
+        const fields = Array.from(e.target.form.elements).filter(filterFormElements)
+        const index = fields.indexOf(e.target)
+        if (fields[index + 1]) {
+          fields[index + 1].focus()
+          e.preventDefault()
+        }
+      }
+    }
+
+    if (formRef.current && isForm(formRef.current)) {
+      formRef.current.addEventListener("keypress", handleKeyPress)
+
+      return formRef.current.removeEventListener("keyPress", handleKeyPress)
+    }
+    console.error("Invalid call: Component using 'useFocusNextOnEnter' hook must be a form element")
+  }, [])
+  return formRef
+}
