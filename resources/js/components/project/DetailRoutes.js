41b1f0aca1ad03545b556cdaf185a649144a2b14
HynekS <hyneksvacha@seznam.cz>
2020-10-11 10:08:44 +0200

    updated import names

--- resources/js/components/project/DetailRoutes.js
+++ resources/js/components/project/DetailRoutes.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 
-import { Detail, Invoices, ExpertSheet, Files } from "./lazyImports"
+import { Detail, InvoiceView, ExpertSheet, Files } from "./lazyImports"
 
 const DetailRoutes = ({ detail }) => {
   return (
     <React.Suspense fallback={<div>I aM tHe SusPEnSE !!!</div>}>
       <Routes>
         <Route path="/" element={<Detail detail={detail} />} />
-        <Route path="/faktury" element={<Invoices detail={detail} />} />
+        <Route path="/faktury" element={<InvoiceView detail={detail} />} />
         <Route path="/expertni-list" element={<ExpertSheet />} />
         <Route path="/nahrane-soubory" element={<Files detail={detail} />} />
       </Routes>
