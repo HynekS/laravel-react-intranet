569783821a90c6a247755977084d91de471588bf
HynekS <hyneksvacha@seznam.cz>
2020-10-11 10:06:29 +0200

    extracted regex to its own module

--- resources/js/services/Date/swapCzDateToISODate.js
+++ resources/js/services/Date/swapCzDateToISODate.js
@@ -1,6 +1,8 @@
+import czechDateRegexp from "./czechDateRegexp"
+
 // WARNING: this function does not care about the input validity.
 export function swapCzDateToISODate(date) {
-  return date.replace(/(\d{1,2})\. ?(\d{1,2})\. ?(\d{4})/, "$3-$2-$1")
+  return date.replace(czechDateRegexp, "$3-$2-$1")
 }
 
 export default swapCzDateToISODate
