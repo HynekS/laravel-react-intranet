7ff74d0176f7926c9a6b7c0a9c89b5d04bd68424
HynekS <hyneksvacha@seznam.cz>
2020-10-11 10:13:57 +0200

    minor style update

--- resources/js/components/project/DetailWrapper.js
+++ resources/js/components/project/DetailWrapper.js
@@ -5,7 +5,7 @@ import tw from "twin.macro"
 
 const DetailWrapper = ({ children }) => (
   <div
-    tw="bg-white relative pt-6 pb-8 px-8 rounded-md rounded-tl-none"
+    tw="bg-white relative pt-6 pb-8 px-4 rounded-md rounded-tl-none lg:(px-8)"
     style={{ boxShadow: "0 10px 15px rgba(0,0,0,0.1), 0 0 6px rgba(0,0,0,0.05)" }}
   >
     {children}
