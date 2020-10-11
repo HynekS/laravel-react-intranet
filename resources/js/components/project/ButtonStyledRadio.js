3bf114e055760761645d6a137770b5ee42091ddd
HynekS <hyneksvacha@seznam.cz>
2020-10-11 10:15:12 +0200

    added recognizable styles

--- resources/js/components/project/ButtonStyledRadio.js
+++ resources/js/components/project/ButtonStyledRadio.js
@@ -3,12 +3,21 @@ import React from "react"
 import { jsx, css } from "@emotion/core"
 import tw from "twin.macro"
 
-const StyledLabel = tw.label`block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4`
-const RowWrapper = tw.div`md:flex md:items-center mb-2`
-const LabelWrapper = tw.div`md:w-1/3`
-const InputWrapper = tw.div`md:w-2/3`
+const DefaultRootWrapper = ({ children }) => <div>{children}</div>
+const DefaultLabelWrapper = ({ children }) => <div>{children}</div>
+const DefaultInputWrapper = ({ children }) => <div>{children}</div>
 
-const Radio = ({ name, options, label, register }) => {
+const ButtonStyledRadio = ({
+  name,
+  options,
+  label,
+  register,
+  RootWrapperComponent = DefaultRootWrapper,
+  LabelWrapperComponent = DefaultLabelWrapper,
+  InputWrapperComponent = DefaultInputWrapper,
+  LabelComponent = "label",
+  ...props
+}) => {
   const renderRadioButtons = (key, index, { length } = list) => {
     return (
       <span key={key} tw="relative">
@@ -36,33 +45,53 @@ const Radio = ({ name, options, label, register }) => {
 
   if (name && options) {
     return (
-      <RowWrapper>
-        <LabelWrapper>
-          <StyledLabel>{label}</StyledLabel>
-        </LabelWrapper>
-        <InputWrapper
+      <div className="fieldWrapper">
+        <div className="labelWrapper">
+          <label htmlFor={name}>{label}</label>
+        </div>
+        <div
+          className="inputWrapper"
           css={css`
             ${tw`text-gray-500`}
-            & input[value="null"]:checked + label {
-              ${tw`text-gray-700 bg-gray-200 border-transparent`}
-              box-shadow: 0 0 0 1px rgba(74, 85, 104, 0.7);
+            & label {
+              transition: all 0.2s ease-in-out;
             }
-            & input[value="0"]:checked + label {
-              ${tw`text-orange-700 bg-orange-200 border-transparent`}
-              box-shadow: 0 0 0 1px rgba(192, 86, 33, 0.7);
+            & input[value="null"] {
+              &:checked + label {
+                ${tw`text-gray-700 bg-gray-200 border-transparent`}
+                box-shadow: 0 0 0 1px rgba(74, 85, 104, 0.7);
+              }
+              &:focus + label {
+                box-shadow: 0 0 0 1px rgba(74, 85, 104, 0.7), 0 0 2px 2px rgba(74, 85, 104, 0.4);
+              }
             }
-            & input[value="1"]:checked + label {
-              ${tw`text-green-700 bg-green-200 border-transparent`}
-              box-shadow: 0 0 0 1px rgba(47, 133, 90, 0.7);
+            & input[value="0"] {
+              &:checked + label {
+                ${tw`text-orange-700 bg-orange-200 border-transparent`}
+                box-shadow: 0 0 0 1px rgba(192, 86, 33, 0.7);
+              }
+              &:focus + label {
+                box-shadow: 0 0 0 1px rgba(192, 86, 33, 0.7), 0 0 2px 2px rgba(192, 86, 33, 0.4);
+              }
+            }
+            & input[value="1"] {
+              &:checked + label {
+                ${tw`text-green-700 bg-green-200 border-transparent`}
+                box-shadow: 0 0 0 1px rgba(47, 133, 90, 0.7);
+              }
+              &:focus + label {
+                box-shadow: 0 0 0 1px rgba(47, 133, 90, 0.7), 0 0 2px 2px rgba(47, 133, 90, 0.4);
+              }
             }
           `}
+          {...props}
         >
           {options && Object.keys(options).map(renderRadioButtons)}
-        </InputWrapper>
-      </RowWrapper>
+        </div>
+      </div>
     )
   }
   return console.warn('"name" and "options" props are required') || null
 }
 
-export default Radio
+export default ButtonStyledRadio
