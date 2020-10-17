327a1ee4564b2b90b3b5802c7593f3a1316bb718
HynekS <hyneksvacha@seznam.cz>
2020-10-17 22:50:59 +0200

    initial commit

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request)
+    {
+        //
+    }
+    public function destroy($id)
+    {
+        //
+    }
+}
