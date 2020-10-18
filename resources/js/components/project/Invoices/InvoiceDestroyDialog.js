147ec5141e4d2791aa9c956e0d13dba9a57e50f3
HynekS <hyneksvacha@seznam.cz>
2020-10-19 00:05:30 +0200

    added some info to warning (via props); minor style updates

--- resources/js/components/project/Invoices/InvoiceDestroyDialog.js
+++ resources/js/components/project/Invoices/InvoiceDestroyDialog.js
@@ -2,25 +2,40 @@
 import React from "react"
 import { jsx, css } from "@emotion/core"
 import tw from "twin.macro"
+import { useDispatch } from "react-redux"
+
+import { deleteInvoice } from "../../../store/invoices"
 
 import SvgExclamation from "../../../vendor/heroicons/outline/Exclamation"
 
-const InvoiceDestroyDialog = ({ modalState, onModalClose, ...props }) => {
+const InvoiceDestroyDialog = ({ modalState: { data }, onModalClose, ...props }) => {
+  const dispatch = useDispatch()
+  const { c_faktury = "", castka = "" } = data
+
+  const handleClick = () => {
+    console.log(data)
+  }
+
   return (
     <div>
       <div tw="flex items-center p-6">
         <div tw="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full text-red-500 bg-red-100 sm:(mx-0 h-10 w-10)">
           <SvgExclamation tw="w-6 stroke-current" />
         </div>
-        <div tw="px-4">Skutečně chcete odstranit fakturu č. XXX?</div>
+        <div tw="px-4">
+          Skutečně chcete odstranit fakturu č. {c_faktury} v hodnotě {castka.toLocaleString("cs-CZ")} Kč?
+        </div>
       </div>
       <footer tw="flex justify-end bg-gray-100 p-6 rounded-lg rounded-t-none">
-        <button tw="bg-gray-100 transition-colors duration-300 text-gray-500 font-medium py-2 px-4  ml-4 rounded hover:(bg-gray-300) focus:(outline-none shadow-outline transition-shadow duration-300)"
-        onClick={onModalClose}
+        <button
+          tw="text-gray-500 font-medium py-2 px-4 ml-4 rounded transition-colors duration-300 hover:(text-gray-600) focus:(outline-none shadow-outline transition-shadow duration-300)"
+          onClick={onModalClose}
         >
           Zrušit
         </button>
-        <button tw="bg-red-600 transition-colors duration-300 text-white font-medium py-2 px-4 ml-4 rounded hover:(bg-red-700) focus:(outline-none shadow-outline transition-shadow duration-300)">
+        <button tw="bg-red-600 transition-colors duration-300 text-white font-medium py-2 px-4 ml-4 rounded hover:(bg-red-700) focus:(outline-none shadow-outline transition-shadow duration-300)"
+          onClick={handleClick}
+        >
           Odstranit fakturu
         </button>
       </footer>
