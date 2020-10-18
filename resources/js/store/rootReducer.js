8e8443fef60998e7e271bb4c60201d925aa85956
HynekS <hyneksvacha@seznam.cz>
2020-10-18 23:57:02 +0200

    imported invoices reducer

--- resources/js/store/rootReducer.js
+++ resources/js/store/rootReducer.js
@@ -4,5 +4,6 @@ import auth from "./auth"
 import projects from "./projects"
 import meta from "./meta"
 import table from "./table"
+import invoice from "./invoices"
 
 export default combineReducers({ app, auth, projects, meta, table })
