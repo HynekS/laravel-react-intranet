0c3d192c2896978275358f06fe92d47447c4e348
HynekS <hyneksvacha@seznam.cz>
2020-10-18 23:53:41 +0200

    created update method

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Faktura;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
@@ -16,10 +17,22 @@ class InvoiceController extends Controller
         //
     }
 
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'castka' => 'required|numeric',
+            'c_faktury' => 'required|numeric',
+            'typ_castky' => 'required|in:"0","1"',
+        ]);
+        $invoice = Faktura::find($id);
+        $invoice->update([
+            'castka' => $request->castka,
+            'c_faktury' => $request->c_faktury,
+            'typ_castky' => (int) $request->typ_castky
+        ]);
+        return $invoice;
     }
+    
     public function destroy($id)
     {
         //
