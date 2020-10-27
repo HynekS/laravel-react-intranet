3613b59e986909e92c06c19519c710a36849b179
HynekS <hyneksvacha@seznam.cz>
2020-10-27 08:32:35 +0100

    significatly refactored: The controller now accepts single file (formerly an array); in case of 'one record models' it deleter older file befor uploading new; in case of 'multi record models' it checks if file exists and if so, it adds increment to its name

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App \ {
+use App\{
     Akce,
     Analyza,
     DigitalizaceNalez,
@@ -15,57 +16,89 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 use function GuzzleHttp\json_decode;
 
 class UploadController extends Controller
-{   
-    // TODO Handle differently database files (because they dont have their own tables)!!!
+{
     public function store($data)
     {
         if (in_array($data->model, ["LAB_databaze", "teren_databaze"])) {
-            $akce = Akce::find($data->id);
-            $firstFilePath = $data->path . $data->filesToUpload[0]->name;
+            $akce = Akce::find($data->projectId);
+            $firstFilePath = $data->path . $data->file->name;
             $akce[$data->model] = $firstFilePath;
             $akce[$data->model . "_vlozil"] = $data->userId;
+            $akce[$data->model . "_vlozeno"] = Carbon::now();
             $akce->save();
 
-            return "Seems we have a {$data->model} which path is {$firstFilePath}";
+            return ["model" => $data->model, "file" => [
+                "file_path" => $firstFilePath,
+                "vlozil" => $data->userId,
+                "vlozeno" => $akce[$data->model . "_vlozeno"],
+            ]];
         }
 
-        $uploadedFiles = $data->filesToUpload;
+        $file = $data->file;
         $model_name = Str::studly(Str::singular($data->model));
         $Model = 'App\\' . $model_name;
-        $savedFiles = [];
 
-        foreach ($uploadedFiles as $file) {
-            // TODO add user id and upload datetime
-            $record = new $Model();
-            $record->file_path = $data->path . $file->name;
-            $record->id_akce = $data->id;
-            $record->vlozil = $data->userId;
-            $record->save();
+        $record = new $Model();
+        $record->file_path = $data->path . $file->name;
+        $record->id_akce = $data->projectId;
+        $record->vlozil = $data->userId;
+        $record->vlozeno = Carbon::now();
+        $record->save();
 
-            $savedFiles[] = $record;
-        }
-        return json_encode($savedFiles);
+        return ["model" => $data->model, "file" => $record];
     }
 
     public function upload(Request $request)
-    {       
+    {
         $data = json_decode($request->data);
-        $uploadedFiles = $data->filesToUpload;
-        $path = $data->id . "/";
+        $file = $data->file;
+        $directory = $data->projectId . "/";
 
-        if(!Storage::exists($path)){
-            Storage::makeDirectory($path);
+        if (!Storage::exists($directory)) {
+            Storage::makeDirectory($directory);
         }
-        $data->path = $path;
+        $data->path = $directory;
+        $base64str = explode(',', $file->content)[1];
 
-        foreach ($uploadedFiles as $file) {
-            $base64str = explode(',', $file->content)[1];
+        if (in_array($data->model, ["LAB_databaze", "teren_databaze"])) {
 
-            Storage::disk('local')->put($path . $file->name, base64_decode($base64str));
+            $akce = Akce::find($data->projectId);
+            // The DBs are part or an Akce row and therefore can store only one record.
+            // It is thus neccessary to remove an old file to prevent orphaned files in storage.
+            if ($akce[$data->model] && Storage::disk('local')->exists($akce[$data->model])) {
+                Storage::disk('local')->delete($akce[$data->model]);
+            }
+
+            Storage::disk('local')->put($directory . $file->name, base64_decode($base64str));
+        } else {
+
+            $uniqueFileName = $this->createUniqueFileName($directory, $file->name);
+            $data->file->name = $uniqueFileName;
+
+            Storage::disk('local')->put($directory . $uniqueFileName, base64_decode($base64str));
         }
         return $this->store($data);
+    }
+
+    public function createUniqueFileName($directory, $filename)
+    {
+
+        if (Storage::disk('local')->exists($directory . $filename)) {
+            $name = pathinfo($filename, PATHINFO_FILENAME);
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+            $duplicateCounter = 1;
+
+            while (Storage::disk('local')->exists($directory . $name . "(" . $duplicateCounter . ")." . $extension)) {
+                $duplicateCounter++;
+            }
+
+            return $name . "(" . $duplicateCounter . ")." . $extension;
+        }
+        return $filename;
     }
 }
