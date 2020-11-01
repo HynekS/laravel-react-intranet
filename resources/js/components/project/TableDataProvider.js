611662d44ef7d00eb10ac3486b7105cb1d3827fa
HynekS <hyneksvacha@seznam.cz>
2020-11-01 19:14:42 +0100

    added shallowEqual argument to selectors returning objects; removed one console.log

--- resources/js/components/project/TableDataProvider.js
+++ resources/js/components/project/TableDataProvider.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo } from "react"
 import { useParams } from "react-router-dom"
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch, shallowEqual } from "react-redux"
 
 import BaseTable from "./BaseTable"
 import "axios-progress-bar/dist/nprogress.css"
@@ -11,8 +11,8 @@ const TableDataProvider = props => {
   const { year } = useParams()
   const dispatch = useDispatch()
 
-  const idsByYear = useSelector(store => store.projects.idsByYear)
-  const allById = useSelector(state => state.projects.byId)
+  const idsByYear = useSelector(store => store.projects.idsByYear, shallowEqual)
+  const allById = useSelector(state => state.projects.byId, shallowEqual)
 
   useEffect(() => {
     const requestYearsNotFetchedAllready = requestYears.filter(year => !idsByYear[year]?.length)
@@ -24,7 +24,7 @@ const TableDataProvider = props => {
   const requestYears = year === undefined ? yearsSince2013 : [year]
 
   const result = useMemo(
-    () => console.log("running memo") ||
+    () =>
       requestYears
         .flatMap(year => idsByYear[year])
         .map(id => allById[id])
