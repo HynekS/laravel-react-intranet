c295945433d70fde5f9079eb11ecfc8bda8a7a10
HynekS <hyneksvacha@seznam.cz>
2020-11-02 22:16:20 +0100

    formatted with prettier

--- resources/js/components/project/DetailPage.js
+++ resources/js/components/project/DetailPage.js
@@ -3,6 +3,8 @@ import React from "react"
 import { jsx } from "@emotion/core"
 import tw from "twin.macro"
 
-const DetailPage = ({ children }) => <div tw="w-full py-2 px-2 bg-gray-400 shadow-inner lg:(px-10)">{children}</div>
+const DetailPage = ({ children }) => (
+  <div tw="w-full py-2 px-2 bg-gray-400 shadow-inner lg:(px-10)">{children}</div>
+)
 
 export default DetailPage
