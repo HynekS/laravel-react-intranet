09f17b65f9becfa63937e16ab72a3e19207f2b84
HynekS <hyneksvacha@seznam.cz>
2020-11-03 07:13:17 +0100

    added webpack chunk name hints

--- resources/js/components/project/lazyImports.js
+++ resources/js/components/project/lazyImports.js
@@ -1,8 +1,8 @@
 import lazy from "react-lazy-with-preload"
 
-const Detail = lazy(() => import("./Detail"))
-const InvoiceView = lazy(() => import("./Invoices/InvoiceView"))
-const ExpertSheet = lazy(() => import("./ExpertSheet"))
-const Files = lazy(() => import("./Files/FilesProvider"))
+const Detail = lazy(() => import(/* webpackChunkName: 'Detail' */ "./Detail"))
+const InvoiceView = lazy(() => import(/* webpackChunkName: 'Invoices' */ "./Invoices/InvoiceView"))
+const ExpertSheet = lazy(() => import(/* webpackChunkName: 'ExpertSheet' */ "./ExpertSheet"))
+const Files = lazy(() => import(/* webpackChunkName: 'Files' */ "./Files/FilesProvider"))
 
 export { Detail, InvoiceView, ExpertSheet, Files }
