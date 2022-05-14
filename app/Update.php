b3520f3621a8f8a807cb206e5706528e6fcde63d
HynekS <hyneksvacha@seznam.cz>
2022-05-14 08:24:12 +0200

    Add relations with other models

--- app/Update.php
+++ app/Update.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Update extends Model
 {
     public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    public function akce()
+    {
+        return $this->belongsTo('App\Akce', 'akce_id', 'id_akce');
+    }
 }
