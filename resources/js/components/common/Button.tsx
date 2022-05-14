133130d40055aaa04894c34d2ca7680a2461e1af
HynekS <hyneksvacha@seznam.cz>
2022-05-14 08:16:09 +0200

    Initial commit

---
+++ resources/js/components/common/Button.tsx
@@ -0,0 +1,24 @@
+import tw from "twin.macro"
+
+type Props = {
+  children: React.ReactNode
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  type?: "button" | "submit" | "reset"
+  className?: string
+} & JSX.IntrinsicElements["button"]
+
+// https://github.com/ben-rogerson/twin.macro/discussions/510#discussioncomment-1203460
+const Button = ({ type = "button", onClick, className, children }: Props) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      css={tw`flex items-center bg-blue-600 transition-colors duration-300 text-white font-medium py-2 px-4 rounded hover:(bg-blue-700) focus:(outline-none ring)`}
+      {...{ className }}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
