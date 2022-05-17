2a0acaf10839f3f5e07c6889affe31bfb5f45a3c
HynekS <hyneksvacha@seznam.cz>
2022-05-17 15:40:52 +0200

    Add rest props ('{...props}')

--- resources/js/components/common/Button.tsx
+++ resources/js/components/common/Button.tsx
@@ -5,16 +5,17 @@ type Props = {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
   type?: "button" | "submit" | "reset"
   className?: string
-} & JSX.IntrinsicElements["button"]
+} & React.HTMLProps<HTMLButtonElement> //& JSX.IntrinsicElements["button"]
 
 // https://github.com/ben-rogerson/twin.macro/discussions/510#discussioncomment-1203460
-const Button = ({ type = "button", onClick, className, children }: Props) => {
+const Button = ({ type = "button", onClick, className, children, ...props }: Props) => {
   return (
     <button
       type={type}
       onClick={onClick}
       css={tw`flex items-center bg-blue-600 transition-colors duration-300 text-white font-medium py-2 px-4 rounded hover:(bg-blue-700) focus:(outline-none ring)`}
       {...{ className }}
+      {...props}
     >
       {children}
     </button>
