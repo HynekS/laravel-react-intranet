bf5bf5da802bcf7f6e0a91e6d305c254c640e425
HynekS <hyneksvacha@seznam.cz>
2022-06-01 17:13:14 +0200

    Initial commit

---
+++ app/Http/Controllers/StatsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Akce;
+
+class StatsController extends Controller
+{
+    public function getStatsByDistricts()
+    {
+        $not_cancelled = Akce::notCancelled()->get();
+
+        $districts = Akce::select('okres')
+            ->distinct('okres')
+            ->get()
+            ->toArray();
+            
+        $all_by_districts = collect($districts)
+            ->map(fn ($d) => [$d['okres'] =>
+            [
+                'all' => $not_cancelled->where('okres', '=', $d['okres'])->count(),
+                'negative' => $not_cancelled->where('okres', '=', $d['okres'])->where('nalez', '=', 0)->count(),
+                'positive' => $not_cancelled->where('okres', '=', $d['okres'])->where('nalez', '=', 1)->count(),
+                '1' => $not_cancelled->where('okres', '=', $d['okres'])->where('id_stav', '=', 1)->count(),
+                '2' => $not_cancelled->where('okres', '=', $d['okres'])->where('id_stav', '=', 2)->count(),
+                '3' => $not_cancelled->where('okres', '=', $d['okres'])->where('id_stav', '=', 3)->count(),
+                '4' => $not_cancelled->where('okres', '=', $d['okres'])->where('id_stav', '=', 4)->count(),
+            ]])
+            ->toArray();
+
+            return response()->json($all_by_districts, 200);
+    }
+
+    public function getStatsByYears()
+    {
+        $by_years = Akce::select(['id_akce', 'rok_per_year', 'okres', 'nalez', 'id_stav'])
+            ->notCancelled()
+            ->get()
+            ->groupBy('rok_per_year')
+            ->map(fn ($y) => collect($y)->groupBy('okres')->toArray());
+
+        $all_by_years = collect($by_years)
+            ->map(fn ($y) => collect($y)
+                ->map(fn ($yd) =>
+                [
+                    'all' => count($yd),
+                    'negative' => count(array_filter($yd, fn ($a) => $a['nalez'] === 0)),
+                    'positive' => count(array_filter($yd, fn ($a) => $a['nalez'] === 1)),
+                    '1' => count(array_filter($yd, fn ($a) => $a['id_stav'] === 1)),
+                    '2' => count(array_filter($yd, fn ($a) => $a['id_stav'] === 2)),
+                    '3' => count(array_filter($yd, fn ($a) => $a['id_stav'] === 3)),
+                    '4' => count(array_filter($yd, fn ($a) => $a['id_stav'] === 4))
+                ])
+                ->toArray());
+
+                return response()->json($all_by_years, 200);
+    }
+}
