7c8e7cb09307c2d036f2ecee307a2d252a296848
HynekS <hyneksvacha@seznam.cz>
2022-10-23 15:50:36 +0200

    Remove dead code

--- app/Http/Transformers/AkceTransformer.php
+++ app/Http/Transformers/AkceTransformer.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Transformers;
 
-use Carbon\Carbon;
-
 class AkceTransformer
 {
   public static function transformResponse($response)
   {
     is_null($response->nalez) ? $response->nalez = "null" : $response->nalez = strval($response->nalez);
-    /*
-    $response->datum_pocatku_text = self::tryToParseDate($response->datum_pocatku_text);
-    $response->datum_ukonceni_text = self::tryToParseDate($response->datum_ukonceni_text);
-    $response->datum_pocatku = self::tryToParseDate($response->datum_pocatku);
-    $response->datum_ukonceni = self::tryToParseDate($response->datum_ukonceni);
-    */
+
     $response->LAB_databaze = $response->LAB_databaze ? [
       0 => [
         'file_path' => $response->LAB_databaze,
@@ -34,18 +27,5 @@ class AkceTransformer
     unset($response['LAB_databaze_vlozeno'], $response['LAB_databaze_vlozil'], $response['teren_databaze_vlozeno'], $response['teren_databaze_vlozil']);
 
     return $response;
-  }
-
-  public static function transformRequest($request)
-  {
-  }
-
-  private static function tryToParseDate($raw_date, $outputFormat = "j. n. Y")
-  {
-    try {
-      return (empty($raw_date) || is_null($raw_date)) ? "" : Carbon::parse($raw_date)->format($outputFormat);
-    } catch (\Exception $err) {
-      return $raw_date;
-    }
   }
 }
