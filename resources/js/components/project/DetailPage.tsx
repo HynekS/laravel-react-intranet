c39bd97f2e56851129b40fbfb09d7a061b41cd3b
HynekS <hyneksvacha@seznam.cz>
2023-04-29 15:08:45 +0200

    Update styles

--- resources/js/components/project/DetailPage.tsx
+++ resources/js/components/project/DetailPage.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 const DetailPage = ({ children }: Props) => (
-  <div tw="w-full py-2 px-2 shadow-inner lg:(px-10)">{children}</div>
+  <div tw="w-full pt-2 pb-8 px-2 bg-gray-100 bg-opacity-75 lg:(px-10)">{children}</div>
 )
 
 export default DetailPage
--- resources/js/components/project/DetailWrapper.tsx
+++ resources/js/components/project/DetailWrapper.tsx
@@ -3,10 +3,7 @@ type Props = {
 }
 
 const DetailWrapper = ({ children }: Props) => (
-  <div
-    tw="bg-white relative pt-6 pb-8 px-4 rounded-md rounded-tl-none w-full lg:(px-8)"
-    style={{ boxShadow: "0 10px 15px rgba(0,0,0,0.1), 0 0 6px rgba(0,0,0,0.05)" }}
-  >
+  <div tw="bg-white border relative pt-6 pb-10 px-4 rounded-md rounded-tl-none w-full lg:(px-8) box-shadow[0 10px 15px rgba(0,0,0,0.05), 0 0 6px rgba(0,0,0,0.04)]">
     {children}
   </div>
 )
